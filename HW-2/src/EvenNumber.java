50d41f51eb092c78b573847bdb75960c3032aff4
Andrey <andy.kislun@gmail.com>
2023-03-22 01:45:15 +0200

    Add solutions for homework tasks

---
+++ HW-2/src/EvenNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введіть число(ціле) для перевірки");
+        int numberFromConsole = scanner.nextInt();
+        System.out.println(checkEven(numberFromConsole));
+    }
+
+    public static String checkEven(int number) {
+        return (number % 2 == 0) ? "Число парне" : "Число не парне";
+    }
+}
