ad1100289e03d6d04f74bdcc34883bb5d1c14b1d
Andrey <andy.kislun@gmail.com>
2023-04-22 11:59:55 +0300

    fix repo

---
+++ src/ua/kyslun/hw4/Task4.java
@@ -0,0 +1,23 @@
+package ua.kyslun.hw4;
+
+import java.util.Arrays;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int[] array = new int[2000];
+        for (int i = 0; i < 2000; i++) {
+            array[i] = (int) (Math.random() * 99 + 1);
+        }
+        System.out.println(Arrays.toString(evenIsZero(array)));
+    }
+
+    public static int[] evenIsZero(int[] array) {
+        int[] calculatingArray = Arrays.copyOf(array, array.length);
+        for (int i = 0; i < calculatingArray.length; i++) {
+            if (calculatingArray[i] % 2 == 0) {
+                calculatingArray[i] = 0;
+            }
+        }
+        return calculatingArray;
+    }
+}
