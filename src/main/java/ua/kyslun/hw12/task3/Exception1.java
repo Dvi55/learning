d7c4764ca21b5f880e238df1a9e11e46d09b393f
Andrii <andy.kislun@gmail.com>
2023-06-19 16:27:37 +0300

    fix inaccuracy

--- src/main/java/ua/kyslun/hw12/task3/Exception1.java
+++ src/main/java/ua/kyslun/hw12/task3/Exception1.java
@@ -2,10 +2,6 @@ package ua.kyslun.hw12.task3;
 
 public class Exception1 extends Exception {
     public Exception1(String message) {
-        try {
-            throw new Exception1("1");
-        } catch (Exception1 e) {
-            throw new RuntimeException(e);
-        }
+        super(message);
     }
 }
--- src/main/java/ua/kyslun/hw12/task3/Exception2.java
+++ src/main/java/ua/kyslun/hw12/task3/Exception2.java
@@ -2,10 +2,6 @@ package ua.kyslun.hw12.task3;
 
 public class Exception2 extends Exception {
     public Exception2(String message) {
-        try {
-            throw new Exception2("2");
-        } catch (Exception2 e) {
-            throw new RuntimeException(e);
-        }
+        super(message);
     }
 }
